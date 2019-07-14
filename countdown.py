34c66d8ca400df62368b21a58d64539d419ad226
drumadrian <adrian.drummond@gmail.com>
2019-07-14 15:17:22 -0500

    refactor and update for Python 3

--- countdown.py
+++ countdown.py
@@ -2,7 +2,6 @@ import time
 import sys
 import os
 
-print "STARTING Your Custom Counter - Down"
 
 
 def is_number(s):
@@ -16,23 +15,20 @@ def is_number(s):
 #Test arguments
 count = sys.argv[1]
 
-
-
 if is_number(count):
-	count = int(float(count))
-	print "STARTING Your Custom Counter"
-	print ""
+    count = int(float(count))
+    print("STARTING Your Custom Counter\n")
 else: 
-	print "You did not enter a number"
-	sys.exit()
+    print("You did not enter a number")
+    sys.exit()
 
 while count !=0:
-	print count
-	time.sleep(1)     #Sleep for 1 second
-	count = count - 1
-	if count%30 == 0:
-		print "Almost there"
-		os.system("tree /usr")
+    print(count)
+    time.sleep(1)     #Sleep for 1 second
+    count = count - 1
+    if count%30 == 0:
+        print("Almost there")
+        os.system("tree /usr")
 
 
 
